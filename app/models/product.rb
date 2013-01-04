11aada39fab17bafd470cece0ac0499eb30cdffc
Brett Hoyer <jbh5079@gmail.com>
2013-01-04 14:18:16 -0500

    writing tests for model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price, :greater_than_or_equal_to => 0.01
+  validates_uniqueness_of :title
+  validates :image_url, :format => {
+  	:with => %r{\.(gif|jpg|png)$}i,
+  	:message => 'Must be URL for GIF, JPG, or PNG image.'
+  }
 end
